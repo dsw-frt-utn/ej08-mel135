5eb9319fab4af9b313269747acb8c0028453ae7b
mel135 <Melani.Barrionuevo@alu.frt.utn.edu.ar>
2026-05-02 01:47:26 -0300

    Ejercicio 2

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal total=0;
+
+            if (quantity <= 0) {
+
+                total = 0;
+            
+            }
+            else
+            {
+                total = quantity * unitPrice;
+            }
+            var resumen = new
+            {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity,
+                Total = total
+            };
+
+            return $"{resumen.Code}-{resumen.Description}-{resumen.Total}";
+
+
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var p2 = new Problema2();
+
+            string resultado = p2.CrearResumenVenta(1001, "Mouse", 2, 1500);
+
+            Console.WriteLine(resultado);
         }
     }
 }
